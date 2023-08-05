5196d49cb50d9b9e6bea16dbc6c8292f4f90c4bb
Dani <git@danny.works>
2023-08-05 13:30:42 -0700

    fixed vs sticky, fixed wins cos webkit don't break it

--- hydrabank.systems/app/Navigation.jsx
+++ hydrabank.systems/app/Navigation.jsx
@@ -34,7 +34,7 @@ import { BiInfoCircle } from "react-icons/bi";
 export const Navigation = ({ params }) => {
     const pathname = usePathname();
     return (
-        <div className={` flex flex-row items-center justify-between gap-x-2 w-full p-4 py-2 md:px-24 2xl:px-96 z-[1] ${pathname === "/" ? "fixed bg-black bg-opacity-[0.75]" : "bg-transparent"}`}>
+        <div className={`flex flex-row items-center justify-between gap-x-2 w-full p-4 py-2 md:px-24 2xl:px-96 z-[1] ${pathname === "/" ? "fixed bg-black bg-opacity-[0.75]" : "bg-transparent"}`}>
             <Link href="/" className="flex flex-row items-center gap-x-2 p-[1px] rounded-full md:rounded-xl hover:bg-neutral-800 transition ease-in-out duration-500">
                 <Image src="/logos/hb.short.transparent.png" width={64} height={64} className="w-12" alt="Hydrabank" />
             </Link>
@@ -85,7 +85,7 @@ export const NavigationItem = ({ href, children, isExternal, key: index }) => {
 export const Top = () => {
     const pathname = usePathname();
     return (
-        <div className={`${pathname === "/" ? "sticky top-0 left-0" : "mb-4"} z-[5]`}>
+        <div className={`${pathname === "/" ? "fixed top-0 left-0" : "mb-4"} w-full z-[5]`}>
             <div className="flex items-center md:justify-center gap-x-4 w-full p-4 bg-gray-900">
                 <span className="">
                     <BiInfoCircle className="text-2xl inline-block text-white mr-2" />
